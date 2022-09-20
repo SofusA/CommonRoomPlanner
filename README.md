7ba403cb739492311f781da19e60fb7f588c13af
Sofus Addington <sofus@addington.dk>
2022-09-20 14:13:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# KanDuSurfe
+Small web service that returns whether or not the weather is suited for kite surfing in the coming week in Copenhagen.
+
+Using YR Api and is implemented in rust, and deployed as an Azure Function 
+
+Can be used as a simple example how to build an Azure Function in Rust. Comments are appreciated.
+
+Cheers
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::net::Ipv4Addr;
+use warp::Filter;
+
+
+#[tokio::main]
+async fn main() {
+    let api = warp::path!("api" / "v1").and_then(todo());
+
+    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
+    let port: u16 = match env::var(port_key) {
+        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
+        Err(_) => 3000,
+    };
+
+    warp::serve(api).run((Ipv4Addr::LOCALHOST, port)).await
+}
